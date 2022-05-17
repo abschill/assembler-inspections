d37f793b10e376a6c0a7c8a2468f1913be5afda6
abschill <ajay198810@gmail.com>
2022-05-16 22:56:44 -0600

    add: ada

--- assembly-out.sh
+++ assembly-out.sh
@@ -1,1 +1,4 @@
+# we use the -S option to tell the compiler to stop after the compilation proper stage, and to not assemble the .s file
+# O3 is an optimization flag
+# optimization flags make the compiler attempt to improve performance and/or code size at the expense of compilation time and possibly the ability to debug the program 
 gcc -S -O3 -fno-asynchronous-unwind-tables $1
---
+++ compile-ada.sh
@@ -0,0 +1,1 @@
+gnatmake hello.adb
